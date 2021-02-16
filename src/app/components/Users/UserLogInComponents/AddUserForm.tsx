2d7939e5fe951b543bdbc06db4f55d2290d50963
Robin <R.leeb@gmx.de>
2021-02-16 14:38:42 +0100

    Show a spinner and compress the picture maximum

--- src/app/components/Users/UserLogInComponents/AddUserForm.tsx
+++ src/app/components/Users/UserLogInComponents/AddUserForm.tsx
@@ -1,7 +1,7 @@
 import { unwrapResult } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux';
 import React, { useEffect, useState } from 'react'
-import { View, Text, TextInput, Button, Image, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, Button, Image, TouchableOpacity, ActivityIndicator } from "react-native";
 import style_userLogIn from './style_userLogIn';
 import { addNewUser } from '../../../../features/users/userSlice';
 import * as ImagePicker from 'expo-image-picker';
@@ -12,6 +12,7 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export const AddUserForm = (props) => {
   const [defaultImage, setDefaultImage] = useState(null);
+  const [loading, setLoading] = useState("");
   const [displayName, setDisplayName] = useState('');
   const [profilePictureURL, setProfilePictureURL] = useState(null)
   const [addRequestStatus, setAddRequestStatus] = useState('idle')
@@ -19,7 +20,9 @@ export const AddUserForm = (props) => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    if (!profilePictureURL) {
+    if(loading === "loading" && !profilePictureURL) {
+      setDefaultImage(<ActivityIndicator size="large" />)
+    } else if (!profilePictureURL) {
       setDefaultImage(
         <View style={style_userLogIn.imgContainer}>
           <Icon name='account' color="lightgrey" size={130} />
@@ -27,7 +30,7 @@ export const AddUserForm = (props) => {
     } else {
       setDefaultImage(<Image source={{ uri: profilePictureURL }} style={style_userLogIn.img} />)
     }
-  }, [profilePictureURL])
+  }, [profilePictureURL, loading])
 
 
   const onDisplayNameDisplayName = e => setDisplayName(e)
@@ -73,16 +76,17 @@ export const AddUserForm = (props) => {
   }
 
   const selectAProfilePicture = async () => {
-
+    
     const permissions = await ImagePicker.requestMediaLibraryPermissionsAsync()
     if (permissions.status !== 'granted') {
       alert("Sorry, we need camera roll permissions to make this work!");
     } else {
+      setLoading("loading")
       let result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
         aspect: [5, 5],
-        quality: 1,
+        quality: 0.1,
       });
       if (!result.cancelled) {
         const storageRef = firebaseApp.storage().ref()
@@ -90,7 +94,6 @@ export const AddUserForm = (props) => {
         const blobFile = await b64toBlob(result.uri)
         await fileRef.put(blobFile)
         setProfilePictureURL(await fileRef.getDownloadURL());
-      
       }
     }
   }
